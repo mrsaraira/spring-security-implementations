5991f11219d5e809c550459727d0e9929020796b
Takhsin <mrsarayra@gmail.com>
2021-04-01 14:54:13 +0300

    upd

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-## Spring Security implementation variants (see branches)
-### main branch uses Basic Auth
+## Spring Security implementation variants
+### This is repository created to explore Spring security different implementations (see branches)
+#### Main branch
+Main branch uses **Basic auth** using default Spring security configurations. To perform a login, you should default username - *user*
+ and a generated password at startup (see console output).
+ 
+ Basically what it does, it requires you to add an *Authorization* header to the REST request with a string value 
+in the format of *Basic username:password*, where **username:password** part encoded in Base64.
--- src/main/java/com/mrsaraira/springsecurityvariants/controllers/DevelopersController.java
+++ src/main/java/com/mrsaraira/springsecurityvariants/controllers/DevelopersController.java
@@ -3,6 +3,7 @@ package com.mrsaraira.springsecurityvariants.controllers;
 import com.mrsaraira.springsecurityvariants.model.Developer;
 import com.mrsaraira.springsecurityvariants.services.DeveloperService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,13 +20,13 @@ public class DevelopersController {
     private final DeveloperService developerService;
 
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Developer>> getDevelopers() {
         return ResponseEntity.ok(developerService.getAllDevelopers());
     }
 
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Developer> getDeveloperById(@PathVariable Long id) {
         return ResponseEntity.ok(developerService.getDeveloper(id));
     }
